b3f24db00ed6b88df250cc5b0b4174df141c57b3
geusfexx <geusfexx@yandex.ru>
2026-02-01 13:15:47 -0500

    Testing-ground - It adds ringbuffer fast.

--- structures/ringbuffer/ringbuffer.cpp
+++ structures/ringbuffer/ringbuffer.cpp
@@ -6,7 +6,6 @@
 *           acq-rel fence
 *           ordinary division
 */
-
 template <typename T>
 class SPSC_RingBufferSlow {
 public:
@@ -38,4 +37,48 @@ private:
     std::size_t capacity;
     std::atomic<std::size_t> head{0};
     std::atomic<std::size_t> tail{0};
+};
+
+/* It has:  False Sharing resolved with alignas
+*           array
+*           acq-rel fence
+*           division using bitwise "AND"
+*/
+template <typename T, std::size_t Capacity>
+class SPSC_RingBufferFast {
+
+    static constexpr std::size_t CacheLine = 64; // Some hardcode :)
+    static constexpr bool isPowerOfTwo(std::size_t n) { return (n != 0) && (n & (n - 1)) == 0; }
+
+    std::size_t increment(std::size_t i) const noexcept {
+        if constexpr (isPowerOfTwo(Capacity)) return (i + 1) & (Capacity - 1);
+        else return (i + 1) % Capacity;
+    }
+
+public:
+    bool push(T val) {
+        std::size_t curr_t = tail.load(std::memory_order_relaxed);
+        std::size_t next = increment(curr_t);
+
+        if (next == head.load(std::memory_order_acquire)) return false;
+        buffer[curr_t] = val;
+        tail.store(next, std::memory_order_release);
+
+        return true;
+    }
+
+    bool pop(T& val) {
+        std::size_t curr_h = head.load(std::memory_order_relaxed);
+
+        if (curr_h == tail.load(std::memory_order_acquire)) return false;
+        val = buffer[curr_h];
+        head.store(increment(curr_h), std::memory_order_release);
+
+        return true;
+    }
+
+private:
+    T buffer[Capacity];
+    alignas(CacheLine) std::atomic<std::size_t> head{0};
+    alignas(CacheLine) std::atomic<std::size_t> tail{0};
 };
