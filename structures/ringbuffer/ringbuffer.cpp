a48e82cdd502479108adc5abde8f3fd6cc3efe03
geusfexx <geusfexx@yandex.ru>
2026-02-02 12:39:27 -0500

    Testing-ground - It adds ringbuffer experimental.

--- structures/ringbuffer/ringbuffer.cpp
+++ structures/ringbuffer/ringbuffer.cpp
@@ -139,3 +139,67 @@ private:
     alignas(CacheLine) std::atomic<std::size_t> head{0};
     std::size_t tail_cache{0}; // local
 };
+
+/* It has:  False Sharing resolved with alignas
+*           array
+*           acq-rel fence
+*           division using bitwise "AND"
+*           fetch_add instead of store
+*/
+template <typename T, std::size_t Capacity>
+class SPSC_RingBufferExperimental {
+
+    static constexpr std::size_t CacheLine = 64; // Some hardcode :)
+    static constexpr bool isPowerOfTwo(std::size_t n) { return (n != 0) && (n & (n - 1)) == 0; }
+    static constexpr std::size_t Mask = Capacity - 1;
+
+    std::size_t constexpr increment(std::size_t i) const noexcept {
+        if constexpr (isPowerOfTwo(Capacity)) return (i + 1) & Mask;
+        else return (i + 1) % Capacity;
+    }
+
+public:
+    bool push(const T& value) {
+        const std::size_t curr_t = tail.load(std::memory_order_relaxed);
+
+        if (curr_t - head_cache >= Capacity) {
+            head_cache = head.load(std::memory_order_acquire); // MB on
+            if (curr_t - head_cache >= Capacity) return false;
+        }
+
+        buffer[curr_t & Mask] = value;
+        tail.fetch_add(1, std::memory_order_release); // MB off
+
+        return true;
+    }
+
+    bool pop(T& value) {
+        const std::size_t curr_h = head.load(std::memory_order_relaxed);
+
+        if (curr_h == tail_cache) {
+            tail_cache = tail.load(std::memory_order_acquire); // MB on
+            if (curr_h == tail_cache) return false;
+        }
+
+        value = buffer[curr_h & Mask];
+        head.fetch_add(1, std::memory_order_release); // MB off
+
+        return true;
+    }
+
+private:
+/*    static constexpr std::size_t get_index(size_t i) noexcept {
+        return i & Mask;
+    }
+*/
+    alignas(CacheLine) T buffer[Capacity];
+
+    // Producer's group
+    alignas(CacheLine) std::atomic<std::size_t> tail{0};
+    std::size_t head_cache{0}; 
+
+    // Consumer's group
+    alignas(CacheLine) std::atomic<std::size_t> head{0};
+    std::size_t tail_cache{0};
+};
+
